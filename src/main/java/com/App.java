51832db048a56ada2de089738a588949addc9035
Tejas Shah <tejasshah2k19@gmail.com>
2024-06-01 14:41:51 +0530

    ioc spring.xml student singelton

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,13 +1,41 @@
 package com;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bean.StudentBean;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+//    	StudentBean s1 = new StudentBean();
+//    	StudentBean s2 = new StudentBean();
+//    	
+//    	System.out.println(s1);
+//    	System.out.println(s2);
+
+		// ioc container ->
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		System.out.println(context);
+		StudentBean s1 = context.getBean("studentBean",StudentBean.class);
+		StudentBean s2 = context.getBean("studentBean",StudentBean.class);
+	
+		System.out.println(s1);
+		System.out.println(s2);
+	}
 }
+
+//spring -> core -> xml -> code 
+//	core bean el context -> same 
+//	class -> singleton -> 
+//		spring container -> IoC container  -> 
+//				1) BeanFactory 
+//				2) ApplicationContext(I)
+//					ClassPathXmlApplicationContext 
+//		1	2	3	4**	5** 
+//		spring.xml 
+
+//web -> zero configuration 
